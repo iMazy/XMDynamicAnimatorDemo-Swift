748658f53a185c439a75c7aa148bd18e9016bc3e
Mazy <twt_ios@163.com>
2016-10-20 08:35:25 +0800

    初始化项目

---
+++ XMDynamicAnimatorDemo/XMDynamicAnimatorDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XMDynamicAnimatorDemo
+//
+//  Created by TwtMac on 16/10/20.
+//  Copyright © 2016年 Mazy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
