59a61eba046135a0836bf18e6df8e51b8250ee97
Mazy <twt_ios@163.com>
2016-10-20 08:37:28 +0800

    初始化项目

---
+++ XMDynamicAnimatorDemo/XMDynamicAnimatorDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMDynamicAnimatorDemo
+//
+//  Created by TwtMac on 16/10/20.
+//  Copyright © 2016年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
